7ce269f5c6f47ac7f6ef6becdf9fa2380505ac9e
Calal.Xaliqov <Calal.Xaliqov@abc-telecom.az>
2024-09-28 18:17:44 +0400

    Added Web Api as Presentation

---
+++ FitnessWorkoutTracker.Persistence/ServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using FitnessWorkoutTracker.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace FitnessWorkoutTracker.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceservices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = string.Empty;
+
+            if (!Debugger.IsAttached)
+                connectionString = Environment.GetEnvironmentVariable("DbFitnessTracker");
+            else
+                connectionString = configuration.GetConnectionString("DbFitnessTracker");
+
+            services.AddDbContext<WorkoutDbContext>(option =>
+            {
+                if (Debugger.IsAttached)
+                {
+                    option.EnableSensitiveDataLogging();
+                    option.EnableDetailedErrors();
+                }
+                option.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
